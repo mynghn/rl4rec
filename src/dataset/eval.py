63aa2e2afb9337cc87b595a30727e8781fd19543
Myounghoon Lee <lmhchris47@snu.ac.kr>
2021-12-01 23:29:39 +0900

    Enable negative relevance

--- src/dataset/eval.py
+++ src/dataset/eval.py
@@ -102,7 +102,7 @@ class AmazonReviewEvalDataset(Dataset):
             )
             .withColumn(
                 "relevance",
-                collect_list("overall").over(
+                collect_list(col("overall") - 3.0).over(
                     W.partitionBy("reviewerID")
                     .orderBy("unixReviewTime")
                     .rowsBetween(W.currentRow, W.unboundedFollowing)
