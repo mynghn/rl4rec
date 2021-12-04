e4740d7e3d7d22be7cd76ae9b8b3ecbdae716409
Myounghoon Lee <lmhchris47@snu.ac.kr>
2021-12-04 18:46:33 +0900

    Rename param

--- src/model/policy.py
+++ src/model/policy.py
@@ -7,25 +7,25 @@ import torch.nn as nn
 class SoftmaxStochasticPolicy(nn.Module):
     def __init__(
         self,
-        n_actions: int,
-        action_embedding_dim: int,
+        n_itmes: int,
+        items_embedding_dim: int,
         adaptive_softmax: bool = False,
         softmax_cutoffs: Sequence = None,
         softmax_temperature: float = 1.0,
     ):
         super(SoftmaxStochasticPolicy, self).__init__()
 
-        self.action_space = torch.arange(n_actions)
+        self.action_space = torch.arange(n_itmes)
         self.action_embeddings = nn.Embedding(
-            num_embeddings=n_actions,
-            embedding_dim=action_embedding_dim,
+            num_embeddings=n_itmes,
+            embedding_dim=items_embedding_dim,
         )
 
         self.adaptive_softmax = adaptive_softmax
         if self.adaptive_softmax is True:
             self.softmax = nn.AdaptiveLogSoftmaxWithLoss(
                 in_features=1,
-                n_classes=n_actions,
+                n_classes=n_itmes,
                 cutoffs=softmax_cutoffs,
             )
         else:
