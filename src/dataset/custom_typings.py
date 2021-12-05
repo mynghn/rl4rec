67d0b85941f70407c3e1ef0c49353454e2ab8227
Myounghoon Lee <lmhchris47@snu.ac.kr>
2021-12-05 19:32:49 +0900

    Implement to method for CUDA on custom data cls

--- src/dataset/custom_typings.py
+++ src/dataset/custom_typings.py
@@ -1,9 +1,13 @@
 from dataclasses import dataclass
 
-from torch import LongTensor
+from torch import LongTensor, device
 
 
 @dataclass
 class PaddedNSortedUserHistoryBatch:
     data: LongTensor
     lengths: LongTensor
+
+    def to(self, device: device):
+        self.data = self.data.to(device)
+        self.lengths = self.lengths.to(device)
