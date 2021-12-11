079d760dc24c9ddcea0e0c5f73f105e25b89d52b
Myounghoon Lee <lmhchris47@snu.ac.kr>
2021-12-12 02:19:39 +0900

    Update train scenario

--- src/command/train.py
+++ src/command/train.py
@@ -15,67 +15,77 @@ def train_agent(
     device: torch.device = torch.device("cpu"),
     debug: bool = True,
 ) -> Optional[Tuple[List[float], List[float]]]:
-    action_policy_loss_log = []
+    agent = agent.to(device)
+    agent.train()
+
+    # 1. Train behavior policy first
     behavior_policy_loss_log = []
     for epoch in range(1, n_epochs + 1):
         start_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        print(f"\nEpoch {epoch} started at: {start_time}\n")
+        print(f"\nEpoch {epoch} for behavior policy train\nstarted at: {start_time}\n")
 
-        agent = agent.to(device)
-        agent.train()
-        for batch_dict in tqdm(train_loader, desc="train"):
-            batch_dict = train_loader.to(batch=batch_dict, device=device)
+        for batch in tqdm(train_loader, desc="train"):
+            batch = train_loader.to(batch=batch, device=device)
 
             # 1. Build State
-            if (
-                agent.state_network.user_feature_enabled
-                and agent.state_network.item_feature_enabled
-            ):
-                state = agent.state_network(
-                    user_history=batch_dict["user_history"],
-                    user_feature_index=batch_dict.get("user_feature_index"),
-                    item_feature_index=batch_dict.get("item_feature_index"),
-                )
-            elif agent.state_network.user_feature_enabled:
-                state = agent.state_network(
-                    user_history=batch_dict["user_history"],
-                    user_feature_index=batch_dict.get("user_feature_index"),
-                )
-            elif agent.state_network.item_feature_enabled:
-                state = agent.state_network(
-                    user_history=batch_dict["user_history"],
-                    item_feature_index=batch_dict.get("item_feature_index"),
-                )
-            else:
-                state = agent.state_network(user_history=batch_dict["user_history"])
-
-            # 2. Compute Policy Losses
-            action_policy_loss = agent.action_policy_loss(
-                state=state,
-                episodic_return=batch_dict["return"],
-                item_index=batch_dict["item_index"],
+            beta_state = agent.beta_state_network(
+                user_history=batch["user_history"],
+                user_feature_index=batch.get("user_feature_index"),
+                item_feature_index=batch.get("item_feature_index"),
             )
+
+            # 2. Compute Policy Loss
             behavior_policy_loss = agent.behavior_policy_loss(
-                state=state, item_index=batch_dict["item_index"]
+                beta_state=beta_state, item_index=batch["item_index"]
             )
 
             # 3. Gradient update agent w/ computed losses
-            agent.update(
-                action_policy_loss=action_policy_loss,
-                behavior_policy_loss=behavior_policy_loss,
-            )
+            agent.update_behavior_policy(behavior_policy_loss=behavior_policy_loss)
 
-            if debug:
-                action_policy_loss_log.append(action_policy_loss.cpu().item())
+            if debug is True:
                 behavior_policy_loss_log.append(behavior_policy_loss.cpu().item())
 
-        if debug:
-            print(
-                f"1. Epoch {epoch}'s final action policy loss: {action_policy_loss.cpu().item()}"
+        if debug is True:
+            print(f"Final behavior policy loss: {behavior_policy_loss.cpu().item()}")
+
+    # 2. Train action policy
+    action_policy_loss_log = []
+    agent.beta_state_network.eval()
+    for epoch in range(1, n_epochs + 1):
+        start_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        print(f"\nEpoch {epoch} for action policy train\nstarted at: {start_time}\n")
+
+        for batch in tqdm(train_loader, desc="train"):
+            batch = train_loader.to(batch=batch, device=device)
+
+            # 1. Build States
+            beta_state = agent.beta_state_network(
+                user_history=batch["user_history"],
+                user_feature_index=batch.get("user_feature_index"),
+                item_feature_index=batch.get("item_feature_index"),
             )
-            print(
-                f"2. Epoch {epoch}'s final behavior policy loss: {behavior_policy_loss.cpu().item()}"
+            pi_state = agent.pi_state_network(
+                user_history=batch["user_history"],
+                user_feature_index=batch.get("user_feature_index"),
+                item_feature_index=batch.get("item_feature_index"),
             )
+
+            # 2. Compute Policy Loss
+            action_policy_loss = agent.action_policy_loss(
+                pi_state=pi_state,
+                beta_state=beta_state,
+                item_index=batch["item_index"],
+                episodic_return=batch["return"],
+            )
+
+            # 3. Gradient update agent w/ computed losses
+            agent.update_action_policy(action_policy_loss=action_policy_loss)
+
+            if debug is True:
+                action_policy_loss_log.append(action_policy_loss.cpu().item())
+
+        if debug is True:
+            print(f"Final action policy loss: {action_policy_loss.cpu().item()}")
 
         print("=" * 80)
 
