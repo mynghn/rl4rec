de077762ffab45e8264f1caf036e9f1bf9a6a034
Myounghoon Lee <lmhchris47@snu.ac.kr>
2021-12-19 19:02:40 +0900

    Implement GRU4Rec

--- src/model/baseline.py
+++ src/model/baseline.py
@@ -1,9 +1,9 @@
-from collections import defaultdict
-from itertools import combinations
-from typing import DefaultDict, Dict, List, Optional, Set, Tuple, Union
+from typing import Dict, List, Optional, Tuple, Union
 
 import numpy as np
 import torch
+import torch.nn as nn
+from dataset.custom_typings import PaddedNSortedUserHistoryBatch
 from pyspark.ml.recommendation import ALS
 from pyspark.sql import DataFrame
 from pyspark.sql import Window as W
@@ -20,149 +20,47 @@ from pyspark.sql.functions import (
 )
 from pyspark.sql.types import FloatType, StructField, StructType
 
-from ..command.eval import build_relevance_book, compute_ndcg
-from ..dataset.retailrocket import RetailrocketDataset
-from ..model.agent import TopKOfflineREINFORCE
+from model.nn import StateTransitionNetwork
 
 
-class JaccardSimilarity:
-    def __init__(self):
-        self.users_by_item_book: DefaultDict[int, Set[str]] = defaultdict(set)
-        self.model: Dict[str, Dict[str, float]] = {}
-
-    def train(self, train_set: RetailrocketDataset):
-        for user, items in train_set.lifetime_items_book.items():
-            for item in items:
-                self.users_by_item_book[item].add(user)
-
-        for item1, item2 in combinations(self.users_by_item_book.keys(), 2):
-            if item1 not in self.model.keys():
-                self.model[item1] = {}
-            if item2 not in self.model.keys():
-                self.model[item2] = {}
-
-            item1_users = self.users_by_item_book[item1]
-            item2_users = self.users_by_item_book[item2]
-
-            intersections = len(item1_users & item2_users)
-            unions = len(item1_users | item2_users)
-
-            self.model[item1][item2] = self.model[item2][item1] = intersections / unions
-
-    def recommend(self, input_items: List[int], K: int) -> List[int]:
-        assert self.model, "Model not been built yet."
-
-        score_book = defaultdict(float)
-        for item_in in input_items:
-            if item_in in self.model.keys():
-                for item_out, sim in self.model[item_in].items():
-                    score_book[item_out] += sim
-
-        top_K = sorted(score_book.items(), key=lambda item: item[1], reverse=True)[:K]
-
-        return [item for item, _ in top_K]
-
-    def get_item_probs(self, input_items: List[int]) -> DefaultDict[int, float]:
-        assert self.model, "Model not been built yet."
-
-        score_book = defaultdict(float)
-        for item_in in input_items:
-            if item_in in self.model.keys():
-                for item_out, sim in self.model[item_in].items():
-                    score_book[item_out] += sim
-
-        ordered = sorted(score_book.items(), key=lambda item: item[1], reverse=True)
-        items = [item for item, _ in ordered]
-        logits = [logit for _, logit in ordered]
-        probs = self.softmax(logits)
-
-        item_prob_book = defaultdict(float)
-        for item, prob in zip(items, probs):
-            item_prob_book[item] = prob
-
-        return item_prob_book
-
-    def softmax(self, logits: List[float]) -> List[float]:
-        numer = np.exp(logits)
-        denom = np.sum(numer)
-        return list(numer / denom)
-
-    def eval(
+class GRU4Rec(nn.Module):
+    def __init__(
         self,
-        agent: TopKOfflineREINFORCE,
-        eval_set: RetailrocketDataset,
-        K: int,
-        discount_factor: float,
-    ) -> Dict[str, float]:
-        agent.to("cpu")
-        agent.eval()
+        n_user_actions: int,
+        user_action_embedding_dim: int,
+        hidden_size: int,
+        output_size: int,
+        padding_signal: int,
+        n_gru_layers: int,
+        dropout: int = 0,
+    ):
+        super(GRU4Rec, self).__init__()
 
-        expected_return_cumulative = 0.0
-        precision_cumulatvie = 0.0
-        recall_cumulative = 0.0
-        ndcg_cumulative = 0.0
-        hit = 0
-        users_hit = set()
+        self.gru_layer = StateTransitionNetwork(
+            n_user_actions=n_user_actions,
+            user_action_embedding_dim=user_action_embedding_dim,
+            hidden_size=hidden_size,
+            padding_signal=padding_signal,
+            num_layers=n_gru_layers,
+            dropout=dropout,
+        )
+        self.output_layer = nn.Sequential(
+            nn.Linear(in_features=hidden_size, out_features=output_size),
+            nn.Tanh(),
+        )
 
-        iter_cnt = 0
-        users_total = set()
-        with torch.no_grad():
-            for idx, data in enumerate(eval_set):
-                (
-                    user_id,
-                    user_history,
-                    actual_seq,
-                    reward_seq,
-                    episodic_return,
-                ) = data
+    def forward(self, user_history: PaddedNSortedUserHistoryBatch) -> torch.FloatTensor:
+        out = self.gru_layer(user_history)
+        out = self.output_layer(out)
+        return
 
-                index_in_history = eval_set.df.iloc[idx]["event_index_in_user_history"]
-                past_item_sequence = eval_set.lifetime_items_book[user_id][
-                    :index_in_history
-                ]
-                # 1. Expected return of model over samples from eval data that follows behavior policy
-                user_history_tensor = torch.LongTensor(user_history).view(1, 1)
-                beta_state = agent.beta_state_network(user_history=user_history_tensor)
-                item_index_tensor = torch.LongTensor(actual_seq[:1]).view(1, 1)
-                behavior_policy_prob = torch.exp(
-                    agent.behavior_policy(beta_state, item_index_tensor)
-                ).item()
-                model_prob = self.get_item_probs(past_item_sequence)[actual_seq[0]]
-                corrected_return = model_prob / behavior_policy_prob * episodic_return
-                expected_return_cumulative += corrected_return
-
-                # 2. Compute metrics from traditional RecSys domain
-                recommendation_list = self.recommend(past_item_sequence, K)
-
-                actual_item_set = set(actual_seq)
-                recommendation_set = set(recommendation_list)
-
-                true_positive = len(actual_item_set & recommendation_set)
-                if true_positive > 0:
-                    hit += 1
-                    users_hit.add(user_id)
-                precision_cumulatvie += true_positive / len(recommendation_set)
-                recall_cumulative += true_positive / len(actual_item_set)
-                ndcg_cumulative += compute_ndcg(
-                    recommendations=recommendation_list,
-                    relevance_book=build_relevance_book(
-                        item_sequence=actual_seq,
-                        reward_sequence=reward_seq,
-                        discount_factor=discount_factor,
-                    ),
-                )
-
-                iter_cnt += 1
-                users_total.add(user_id)
-
-        return {
-            "E[Return]": expected_return_cumulative / iter_cnt,
-            f"Precision at {K}": precision_cumulatvie / iter_cnt,
-            f"Recall at {K}": recall_cumulative / iter_cnt,
-            f"nDCG at {K}": ndcg_cumulative / iter_cnt,
-            "Hit Rate": hit / iter_cnt,
-            "User Hit Rate": len(users_hit) / len(users_total),
-        }
+    # Implement TOP1 loss from the paper
+    def loss(
+        self, logits: torch.FloatTensor, relevant_logit: float
+    ) -> torch.FloatTensor:
+        return torch.mean(
+            torch.sigmoid(logits - relevant_logit) + torch.sigmoid(logits ** 2)
+        )
 
 
 class CollaborativeFiltering:
